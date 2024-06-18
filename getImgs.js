28432ee44980494ada8a217f04877abbff6d5ba5
Alina <nikitina@terpmail.umd.edu>
2024-06-18 10:10:35 -0400

    add 2022 photos and further automate image addition

--- getImgs.js
+++ getImgs.js
@@ -1,11 +1,11 @@
-// number of images
-const numberOfImages = 125; // change this to the total number of images you have
-const year = 2023;
 const imageContainer = document.getElementById('image-container');
+const yearToPhotoCount = {2023: 125, 2022: 130};
 
-for (let i = numberOfImages; i > 0; i--) {
-    const img = document.createElement('img');
-    img.src = `photolog/${year}/${i}.jpeg`;
-    img.alt = `Image ${i}`;
-    imageContainer.appendChild(img);
-}
+Object.keys(yearToPhotoCount).map(Number).sort((a, b) => b - a).forEach(year => {
+    for (let i = yearToPhotoCount[year]; i > 0; i--) {
+        const img = document.createElement('img');
+        img.src = `photolog/${year}/${i}.jpeg`;
+        img.alt = `Image ${i}`;
+        imageContainer.appendChild(img);
+    }
+});
