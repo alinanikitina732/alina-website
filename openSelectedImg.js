841c347b4546f1cdf2ab7c0a72391bb521b88b0d
Alina <nikitina@terpmail.umd.edu>
2024-06-19 17:39:38 -0400

    fit single image view to small screen

--- openSelectedImg.js
+++ openSelectedImg.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const imageUrl = this.src;
             // remember scroll position
             localStorage.setItem('scrollpos', window.scrollY || window.pageYOffset);
-            window.open(`image-viewer.html?src=${encodeURIComponent(imageUrl)}`, '_blank');
+            window.open(`image-viewer.html?src=${encodeURIComponent(imageUrl)}`, '_self');
         });
     });
 });
