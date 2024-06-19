88af08e58f7c9ec01670c3a808e12a08a7fb352d
Alina <nikitina@terpmail.umd.edu>
2024-06-19 00:50:54 -0400

    change jpeg resolutions

--- getImgs.js
+++ getImgs.js
@@ -1,5 +1,5 @@
 const imageContainer = document.getElementById('image-container');
-const yearToPhotoCount = {2024: 87, 2023: 125, 2022: 130, 2021: 42, 2020: 43};
+const yearToPhotoCount = {2024: 136, 2023: 122, 2022: 131, 2021: 42, 2020: 43};
 
 Object.keys(yearToPhotoCount).map(Number).sort((a, b) => b - a).forEach(year => {
     for (let i = yearToPhotoCount[year]; i > 0; i--) {
