32fa33c81e857a5d5ab1d0422a0c23c10b71091d
Alina <nikitina@terpmail.umd.edu>
2024-11-13 08:31:32 -0800

    change num of photos in 2024

--- getImgs.js
+++ getImgs.js
@@ -1,5 +1,5 @@
 const imageContainer = document.getElementById('image-container');
-const yearToPhotoCount = {2024: 138, 2023: 122, 2022: 128, 2021: 43, 2020: 43};
+const yearToPhotoCount = {2024: 247, 2023: 122, 2022: 128, 2021: 43, 2020: 43};
 
 Object.keys(yearToPhotoCount).map(Number).sort((a, b) => b - a).forEach(year => {
     for (let i = yearToPhotoCount[year]; i > 0; i--) {
