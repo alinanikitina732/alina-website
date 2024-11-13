52f0a4379342857b695509ade3b5733b982800cd
Alina <nikitina@terpmail.umd.edu>
2024-11-13 00:30:54 -0800

    add arrows to click between photos

--- openSelectedImg.js
+++ openSelectedImg.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     const images = document.querySelectorAll('.gallery-image');
 
     images.forEach(image => {
         image.addEventListener('click', function() {
             const imageUrl = this.src;
-            // remember scroll position
-            localStorage.setItem('scrollpos', window.scrollY || window.pageYOffset);
             window.open(`image-viewer.html?src=${encodeURIComponent(imageUrl)}`, '_self');
         });
     });
+
+    var scrollpos = localStorage.getItem('scrollpos');
+    if (scrollpos) window.scrollTo(0, scrollpos);
+
 });
+
+window.onbeforeunload = function(e) {
+    localStorage.setItem('scrollpos', window.scrollY);
+};
