679273315db68d5dc3d29348b9277821b54cce59
Alina <nikitina@terpmail.umd.edu>
2024-12-30 01:38:59 -0500

    add more 2024 photos

--- getImgs.js
+++ getImgs.js
@@ -1,12 +1,24 @@
 const imageContainer = document.getElementById('image-container');
-const yearToPhotoCount = {2024: 247, 2023: 122, 2022: 128, 2021: 43, 2020: 43};
+const yearToPhotoCount = {2024: 292, 2023: 122, 2022: 128, 2021: 43, 2020: 43};
 
 Object.keys(yearToPhotoCount).map(Number).sort((a, b) => b - a).forEach(year => {
-    for (let i = yearToPhotoCount[year]; i > 0; i--) {
-        const img = document.createElement('img');
-        img.classList.add('gallery-image');
-        img.src = `photolog/${year}/${i}.jpeg`;
-        img.alt = `Image ${i}`;
-        imageContainer.appendChild(img);
+    
+    if (year == 2024) {
+        for (let i = 1; i <= yearToPhotoCount[year]; i++) {
+            const img = document.createElement('img');
+            img.classList.add('gallery-image');
+            img.src = `photolog/${year}/${i}.jpeg`;
+            img.alt = `Image ${i}`;
+            imageContainer.appendChild(img);
+        }
+    } else {
+        for (let i = yearToPhotoCount[year]; i > 0; i--) {
+            const img = document.createElement('img');
+            img.classList.add('gallery-image');
+            img.src = `photolog/${year}/${i}.jpeg`;
+            img.alt = `Image ${i}`;
+            imageContainer.appendChild(img);
+        }
     }
+    
 });
