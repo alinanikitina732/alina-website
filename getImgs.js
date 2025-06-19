2c432047cea3ed1c524c9135124c521ca64e2ba7
Alina <nikitina@terpmail.umd.edu>
2025-06-18 22:57:23 -0700

    [fix]: remove 2024 conditional

--- getImgs.js
+++ getImgs.js
@@ -3,22 +3,12 @@ const yearToPhotoCount = {2025: 211, 2024: 292, 2023: 122, 2022: 128, 2021: 43, 
 
 Object.keys(yearToPhotoCount).map(Number).sort((a, b) => b - a).forEach(year => {
     
-    if (year == 2024) {
-        for (let i = 1; i <= yearToPhotoCount[year]; i++) {
-            const img = document.createElement('img');
-            img.classList.add('gallery-image');
-            img.src = `photolog/${year}/${i}.jpeg`;
-            img.alt = `Image ${i}`;
-            imageContainer.appendChild(img);
-        }
-    } else {
-        for (let i = yearToPhotoCount[year]; i > 0; i--) {
-            const img = document.createElement('img');
-            img.classList.add('gallery-image');
-            img.src = `photolog/${year}/${i}.jpeg`;
-            img.alt = `Image ${i}`;
-            imageContainer.appendChild(img);
-        }
+    for (let i = yearToPhotoCount[year]; i > 0; i--) {
+        const img = document.createElement('img');
+        img.classList.add('gallery-image');
+        img.src = `photolog/${year}/${i}.jpeg`;
+        img.alt = `Image ${i}`;
+        imageContainer.appendChild(img);
     }
     
 });
